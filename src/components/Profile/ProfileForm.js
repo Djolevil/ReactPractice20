9cb0abc1eeaee7f75afe19c6a2d2768f34276840
Djordje Knezevic <starwarrior@mycity.rs>
2022-07-16 12:02:52 +0200

    Redirect user after login and password change

--- src/components/Profile/ProfileForm.js
+++ src/components/Profile/ProfileForm.js
@@ -1,8 +1,11 @@
 import { useRef, useContext } from 'react';
+import { useHistory } from 'react-router-dom';
 import AuthContext from '../../store/auth-context';
 import classes from './ProfileForm.module.css';
 
 const ProfileForm = () => {
+  const history = useHistory();
+  
   const newPasswordInputRef = useRef();
   const authCtx = useContext(AuthContext);
 
@@ -24,6 +27,7 @@ const ProfileForm = () => {
     }).then(res => {
       // assume, for now, that it always succeds
 
+      history.replace('/');
     });
   };
   return (
